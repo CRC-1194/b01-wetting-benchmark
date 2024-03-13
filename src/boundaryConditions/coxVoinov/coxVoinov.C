e1717f3cc684aab4d49c078c2319800b2aecf608
hassan.asghar@tu-darmstadt.de <hassan.asghar@tu-darmstadt.de>
2024-03-13 07:14:27 +0100

    Capillary number calc using PLIC-interp velocity

--- src/boundaryConditions/coxVoinov/coxVoinov.C
+++ src/boundaryConditions/coxVoinov/coxVoinov.C
@@ -54,12 +54,17 @@ Developed by:
 
 #include <functional>
 
+#include "interpolationCellPoint.H"
+
 // * * * * * * * * * * * Private Member Functions * * * * * * * * * * * * * * //
 
-// Check if the patch face has a contact line: based on signed distance calculations
-bool Foam::coxVoinov::hasContactLine(label faceI) const
+bool Foam::coxVoinov::
+hasContactLine
+(
+    label faceI
+) const
 {
-    // Look up PLIC normals and positions. 
+        // Look up PLIC normals and positions. 
     const auto& db = this->db(); 
 
     const auto normalsName = IOobject::groupName
@@ -94,7 +99,6 @@ bool Foam::coxVoinov::hasContactLine(label faceI) const
 
     const volVectorField& interfaceCentre = 
         db.lookupObject<volVectorField>(centresName);
-
 
     // Get patch fields for interface normals and centers
     const fvPatch& patch = this->patch();
@@ -284,61 +288,29 @@ Foam::coxVoinov::theta
     const fvsPatchVectorField& nHat
 ) const
 {
-        //patch face normal vectors
-        const vectorField nf(patch().nf());
-
-        // Calculated the component of the velocity parallel to the wall
-        vectorField Uwall(Up.patchInternalField() - Up);
-
-       
-        Uwall -= (nf & Uwall) * nf;
-
-        // Find the direction of the interface parallel to the wall
-        vectorField nWall(nHat - (nf & nHat) * nf);
-
-        // Normalise nWall
-        nWall /= (mag(nWall) + SMALL);
-
-
-        // Calculate Uwall resolved normal to the interface parallel to
-        // the interface
-        scalarField uwall(-nWall & Uwall);
- 
-
-        const label patchi = this -> patch().index();
-
-        const volScalarField & nu1 =
-            this -> db().objectRegistry::lookupObject < volScalarField > ("nu1");
-
-        const dictionary & transportProperties =
-            this -> db().objectRegistry::lookupObject < IOdictionary >
-            (
-                "transportProperties"
-            );
-
-        word phase1Name(wordList(transportProperties.lookup("phases"))[0]);
-        word phase2Name(wordList(transportProperties.lookup("phases"))[1]);
-
-
-        dimensionedScalar rho1(transportProperties.subDict(phase1Name).getScalar("rho"));
-        dimensionedScalar sigmap(transportProperties.getScalar("sigma"));
-
-
-        const fvPatchScalarField & nu1p = nu1.boundaryField()[patchi];
-      
-       //Capillary number fields
-        scalarField Ca(nu1p * rho1.value() * uwall / sigmap.value());
-
-    // Compute the contact angles at the wall.
-    tmp<scalarField> thetafTmp = Foam::radToDeg(Foam::acos(nHat & nf));
-    scalarField& thetaf = thetafTmp.ref();
-
-    // Visualization.
+    //Patch Index
     const auto& patch = this->patch();
     const label patchIndex =patch.index();
-    auto& clangleBoundaryField = contactLineAngle_.boundaryFieldRef();
-    auto& clanglePatchField = clangleBoundaryField[patchIndex];
-    clanglePatchField *= 0.0;
+
+    //patch face normal vectors
+    const vectorField nf(patch.nf());
+
+    const volScalarField & nu1 =
+        this -> db().objectRegistry::lookupObject < volScalarField > ("nu1");
+
+    const dictionary & transportProperties =
+        this -> db().objectRegistry::lookupObject < IOdictionary >
+        (
+            "transportProperties"
+        );
+
+    word phase1Name(wordList(transportProperties.lookup("phases"))[0]);
+    word phase2Name(wordList(transportProperties.lookup("phases"))[1]);
+
+    dimensionedScalar rho1(transportProperties.subDict(phase1Name).getScalar("rho"));
+    dimensionedScalar sigmap(transportProperties.getScalar("sigma"));
+
+    const fvPatchScalarField & nu1p = nu1.boundaryField()[patchIndex];
 
     word fieldName = "alpha." + phase1Name;
     const volScalarField& alpha1_ =
@@ -348,36 +320,142 @@ Foam::coxVoinov::theta
     // Lookup the desired alpha values on the patch
     const fvPatchField<scalar>& alphaPatchField = abf[patchIndex];
     
+    //Initialization without velocity which is updated later in the code
+    //Capillary number fields
+    scalarField Ca(nu1p * rho1.value() / sigmap.value());
+
+    // 1. Get the PLIC centre and normals
+    // Look up PLIC normals and positions. 
+    const auto& db = this->db(); 
+
+    const auto normalsName = IOobject::groupName
+    (
+        "interfaceNormal", 
+        this->internalField().group()
+    );
+    const auto centresName = IOobject::groupName
+    (
+        "interfaceCentre", 
+        this->internalField().group()
+    );
+
+    bool hasNormals = db.found(normalsName);
+    bool hasCentres = db.found(centresName);
+
+    //If there are no PLIC elements i.e, no wall velocity making Capillary number equals zero
+    // Else calculate Ca using interpolated cell centred velocity to PLIC centre (using inverse distance weightings)
+    if (!hasNormals || !hasCentres )
+    {
+        // This BC is updated before interface reconstruction.
+        // Do nothing if PLIC fields are not available in the registry. 
+
+        // Calculated the component of the velocity parallel to the wall
+        vectorField Uwall(Up.patchInternalField() - Up);
+        //Info << "Patch Internal Field: \n " << Uwall<< nl;
+
+        //Info << "\n Patch internal field: \n" << Up.patchInternalField();
+        Uwall -= (nf & Uwall) * nf;
+
+        // Find the direction of the interface parallel to the wall
+        vectorField nWall(nHat - (nf & nHat) * nf);
+
+        // Normalise nWall
+        nWall /= (mag(nWall) + SMALL);
+
+        // Calculate Uwall resolved normal to the interface parallel to
+        // the interface
+        scalarField uwall(-nWall & Uwall);
+
+        //Capillary number fields
+        Ca = nu1p * rho1.value() * uwall / sigmap.value();
+    }
+    else
+    {
+        const volVectorField& interfaceCentre = 
+            db.lookupObject<volVectorField>(centresName);
+
+        // Cell-centred velocity field
+        Foam::volVectorField& U  = const_cast<Foam::volVectorField&> (this -> db().objectRegistry::lookupObject < volVectorField > ("U"));
+
+        // velocity field at the plic centre
+        // will be updated using inverse distance interpolation scheme on U
+        //This field is then used to calculate the Capillary number as in isoAdvection.C
+        // plicU is used for advection
+        Foam::volVectorField Uplic (U*scalar(0));
+
+        // Create object for interpolating velocity to isoface centres
+        interpolationCellPoint<vector> UInterp(U);
+
+        //Interpolate the cell-centred velocity to the PLIC-centre
+        forAll(interfaceCentre, cellI)
+        {
+            Uplic[cellI] = UInterp.interpolate(interfaceCentre[cellI], cellI);
+        }
+        //Get patch fields for PLIC velocity
+        const auto& pUplic = Uplic.boundaryField()[patchIndex];
+
+        // Calculated the component of the velocity parallel to the wall
+        vectorField Uwall(pUplic.patchInternalField() - pUplic);
+        //Info << "Patch Internal Field: \n " << Uwall<< nl;
+
+        //Info << "\n Patch internal field: \n" << Up.patchInternalField();
+        Uwall -= (nf & Uwall) * nf;
+
+        // Find the direction of the interface parallel to the wall
+        vectorField nWall(nHat - (nf & nHat) * nf);
+
+        // Normalise nWall
+        nWall /= (mag(nWall) + SMALL);
+
+        // Calculate Uwall resolved normal to the interface parallel to
+        // the interface
+        scalarField uwall(-nWall & Uwall);
+
+        //Capillary number fields
+        Ca = nu1p * rho1.value() * uwall / sigmap.value();
+    }
+
+    // Compute the contact angles at the wall.
+    tmp<scalarField> thetafTmp = Foam::radToDeg(Foam::acos(nHat & nf));
+    scalarField& thetaf = thetafTmp.ref();   
+
+    // Visualization.
+    auto& clangleBoundaryField = contactLineAngle_.boundaryFieldRef();
+    auto& clanglePatchField = clangleBoundaryField[patchIndex];
+    clanglePatchField *= 0.0;
+        
+    // Visualization of the contact angle
+    const fvMesh& mesh = nu1.mesh(); 
+    const auto& faceOwner = mesh.faceOwner();     
+
     // For all boundary faces
     forAll(thetaf, faceI)
     {    
-        // If we are in a contact-line cell
-        if (hasContactLine(faceI) && alphaPatchField[faceI] > 1e-5)
+        if (hasContactLine(faceI) )
         {
+            // Visualize current PLIC contact angle as a cell-centered value.
+            contactLineAngle_[faceOwner[patch.start() + faceI]] = thetaf[faceI];
+
             thetaf[faceI] = min(
-            180 / constant::mathematical::pi * (pow(beta_ * pos(Ca[faceI]) * Ca[faceI] +
+            180 / constant::mathematical::pi * (pow(beta_  * mag(Ca[faceI]) +
                 pow(theta0_ * constant::mathematical::pi / 180, 3),
                 0.3333333)),
             scalar(135)
         );
             clanglePatchField[faceI] = thetaf[faceI];
-            // Pout << "Contact line on face " << faceI
-            //      << "Cell ID " << nu1.mesh().faceOwner()[faceI + this->patch().start()]
-            //      << "\n\ttheta = " << thetaf[faceI]
-            //      //<< "\n\tnWall = " << nWall[faceI]
-            //      //<< "\n\tuwall = " << uwall[faceI]
-            //      << "\n\tCa = " << Ca[faceI]
-            //      <<endl;
         }
     }
 
-    return min(
-            180 / constant::mathematical::pi * (pow(beta_ * pos(Ca) * Ca +
-                pow(theta0_ * constant::mathematical::pi / 180, 3),
-                0.3333333)),
-            scalar(135)
-        );
+    return thetafTmp;
+    // return min(
+    //         180 / constant::mathematical::pi * (pow(beta_ * pos(Ca) * Ca +
+    //             pow(theta0_ * constant::mathematical::pi / 180, 3),
+    //             0.3333333)),
+    //         scalar(135)
+    //     );
+
 }
+
 
 void Foam::coxVoinov::write(Ostream& os) const
 {
